a798524940535668921ba092e67c6dbcbbd6f602
wilkflow(Savva Petrov) <115888484+wilkflow@users.noreply.github.com>
2025-07-30 15:05:25 -0400

    Update pipe.bat

--- pipe.bat
+++ pipe.bat
@@ -2,5 +2,5 @@ for i in {1..10}; do
 	echo "Looping..."
 	curl https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt -L -o http.txt &
 	wait
-	(echo "LINK-TO-VISIT") | python SleekWebBot.py
+	(echo "LINK-TO-VISIT") | python webBot2.py
 done
